c84a5e99fed5a6bc15624187255f866e4a4418ee
sschneeberg <simone.m.schneeberg@gmail.com>
2020-12-09 12:21:46 -0500

    Add form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import "./App.css";
-import Post from "./Post";
+import './App.css';
+import Post from './Post';
 
 function App(props) {
-  return (
-    <div className="App">
-      <Post
-        title={props.post.title}
-        author={props.post.author}
-        body={props.post.body}
-        comments={props.post.comments}
-      />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Post
+                title={props.post.title}
+                author={props.post.author}
+                body={props.post.body}
+                comments={props.post.comments}
+            />
+        </div>
+    );
 }
 
 export default App;
--- src/Post.js
+++ src/Post.js
@@ -1,13 +1,27 @@
 //to use Class components props are this.props
 import React, { Component } from 'react';
+import './App.css';
 
 class Post extends Component {
     constructor(props) {
         super(props);
-        this.state = { body: this.props.body };
+        this.state = { body: this.props.body, show: 'show' };
     }
 
-    render(props) {
+    edit(evt) {
+        let newBody = evt.target[0].value;
+        this.setState({ body: newBody });
+    }
+
+    show() {
+        if (this.state.show === '') {
+            this.setState({ show: 'show' });
+        } else {
+            this.setState({ show: '' });
+        }
+    }
+
+    render() {
         const comments = this.props.comments.map((comment) => {
             return <p className="comments">{comment}</p>;
         });
@@ -17,6 +31,22 @@ class Post extends Component {
                     <h1>{this.props.title}</h1>
                     <h3>{this.props.author}</h3>
                     <p>{this.state.body}</p>
+                    <button
+                        onClick={() => {
+                            this.show();
+                        }}>
+                        edit
+                    </button>
+                    <form
+                        className={this.state.show}
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            this.edit(e);
+                            this.show();
+                        }}>
+                        <input type="text" />
+                        <button type="submit">SAVE</button>
+                    </form>
                 </article>
                 <hr />
                 <h3>Comments:</h3>
