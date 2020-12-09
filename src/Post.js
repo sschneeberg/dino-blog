2dac71d2ef9986db0988445e5a6019ae06150c35
sschneeberg <simone.m.schneeberg@gmail.com>
2020-12-09 11:48:35 -0500

    Change to class component

--- src/Post.js
+++ src/Post.js
@@ -1,21 +1,29 @@
 //to use Class components props are this.props
+import React, { Component } from 'react';
 
-function Post(props) {
-  const comments = props.comments.map((comment) => {
-    return <p className="comments">{comment}</p>;
-  });
-  return (
-    <div className="Post">
-      <article className="post">
-        <h1>{props.title}</h1>
-        <h3>{props.author}</h3>
-        <p>{props.body}</p>
-      </article>
-      <hr />
-      <h3>Comments:</h3>
-      {comments}
-    </div>
-  );
+class Post extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { body: this.props.body };
+    }
+
+    render(props) {
+        const comments = this.props.comments.map((comment) => {
+            return <p className="comments">{comment}</p>;
+        });
+        return (
+            <div className="Post">
+                <article className="post">
+                    <h1>{this.props.title}</h1>
+                    <h3>{this.props.author}</h3>
+                    <p>{this.state.body}</p>
+                </article>
+                <hr />
+                <h3>Comments:</h3>
+                {comments}
+            </div>
+        );
+    }
 }
 
 export default Post;
